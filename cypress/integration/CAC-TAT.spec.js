36316dd8a901c7ca18056a028c694fdfda53bfd2
leonardodognani <leonarddewar@outlook.com>
2023-05-02 14:13:09 -0300

    study until  exercices tests number 6 in cypress, resolved

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -17,13 +17,75 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
 
-    it.only('preenche os campos obrigatórios e envia o formulário', function() {
+    it('preenche os campos obrigatórios e envia o formulário', function() {
+        const longText = 'Teste, teste, teste, Teste, teste, teste, Teste, teste, teste, Teste, teste, teste, Teste, teste, teste, Teste, teste, teste, Teste, teste, teste, Teste, teste, teste, Teste, teste, teste, Teste, teste, teste, Teste, teste, teste, Teste, teste, teste, Teste, teste, teste, Teste, teste, teste, Teste, teste, teste, Teste, teste, teste, Teste, teste, teste, Teste, teste, teste, Teste, teste, teste, Teste, teste, teste, Teste, teste, teste, Teste, teste, teste,'
         cy.get('#firstName').type('Leo')
         cy.get('#lastName').type('Dognani')
         cy.get('#email').type('leo@exemplo.com')
-        cy.get('#open-text-area').type('Teste')
+        cy.get('#open-text-area').type(longText, { delay:0 })
         cy.get('button[type="submit"]').click()
 
         cy.get('.success').should('be.visible')
+    })
+
+    it.only('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function() {
+        cy.get('#firstName').type('Leo')
+        cy.get('#lastName').type('Dognani')
+        cy.get('#email').type('leo@exemplo,com')
+        cy.get('#open-text-area').type('teste')
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.error').should('be.visible')
+    })
+
+    it('campo de telefone continua vazio quando preenchido com valor não numérico', function() {
+        cy.get('#phone')
+        .type('abcdefghij')
+        .should('have.value', '')
+    })
+
+    it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function() {
+        cy.get('#firstName').type('Leo')
+        cy.get('#lastName').type('Dognani')
+        cy.get('#email').type('leo@exemplo.com')
+        cy.get('#phone-checkbox').click()
+        cy.get('#open-text-area').type('teste')
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.error').should('be.visible')
+    })
+
+    it('preenche e limpa os campos nome, sobrenome, email e telefone', function(){
+        cy.get('#firstName')
+        .type('Leo')
+        .should('have.value', 'Leo')
+        .clear()
+        .should('have.value', '')
+        cy.get('#lastName')
+        .type('Dognani')
+        .should('have.value', 'Dognani')
+        .clear()
+        .should('have.value', '')
+        cy.get('#email')
+        .type('leo@exemplo.com')
+        .should('have.value', 'leo@exemplo.com')
+        .clear()
+        .should('have.value', '')
+        cy.get('#phone')
+        .type('1234567890')
+        .should('have.value', '1234567890')
+        .clear()
+        .should('have.value', '')
+        cy.get('#open-text-area')
+        .type('teste')
+        .should('have.value', 'teste')
+        .clear()
+        .should('have.value', '')
+    })
+
+    it.only('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios.', function() {
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.error').should('be.visible')
     })
   })
